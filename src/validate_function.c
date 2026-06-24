94808454b5e15b73edc123dc4caa817e6db1d7de
Andre.Gil1 <andre.gil@genomicsengland.co.uk>
2026-06-24 10:19:56 +0100

    chore: Update second parameter of validate function to only accept objects

--- src/validate_function.c
+++ src/validate_function.c
@@ -12,38 +12,16 @@ PHP_FUNCTION(validate)
     
     ZEND_PARSE_PARAMETERS_START(2, 4)
         Z_PARAM_ARRAY(rawData)
-        Z_PARAM_ZVAL(model)
+        Z_PARAM_OBJECT(model)
         Z_PARAM_OPTIONAL
         Z_PARAM_BOOL(strict)
         Z_PARAM_BOOL(stopAtFirstError)
     ZEND_PARSE_PARAMETERS_END();
     
-    // For now, just create and return a new instance of the model class
+    // For now, just return a clone of the model object
     // This is a stub implementation
-    if (Z_TYPE_P(model) == IS_STRING) {
-        zend_string *class_name = zval_get_string(model);
-        zend_class_entry *ce = zend_lookup_class(class_name);
-        zend_string_release(class_name);
-        
-        if (!ce) {
-            zend_throw_exception(zend_ce_exception, "Class not found", 0);
-            RETURN_FALSE;
-        }
-        
-        // Create new instance
-        zval instance;
-        object_init_ex(&instance, ce);
-        
-        // For now, just return the instance without actual validation
-        RETURN_ZVAL(&instance, 0, 1);
-    } else if (Z_TYPE_P(model) == IS_OBJECT) {
-        // If model is already an object, return a clone
-        zval clone;
-        ZVAL_OBJ(&clone, Z_OBJ_P(model));
-        Z_ADDREF(clone);
-        RETURN_ZVAL(&clone, 0, 1);
-    }
-    
-    zend_throw_exception(zend_ce_exception, "Invalid model parameter", 0);
-    RETURN_FALSE;
+    zval clone;
+    ZVAL_OBJ(&clone, Z_OBJ_P(model));
+    Z_ADDREF(clone);
+    RETURN_ZVAL(&clone, 0, 1);
 }
