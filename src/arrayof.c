f7f08ba6dfc56f34b77a231f303c0c1570817115
Andre.Gil1 <andre.gil@genomicsengland.co.uk>
2026-07-21 15:41:54 +0100

    chore: Update ArrayOf to match new generated stub

--- src/arrayof.c
+++ src/arrayof.c
@@ -4,9 +4,9 @@
 #include "zend_variables.h"
 #include "Zend/zend_exceptions.h"
 
-zend_class_entry *Attributes_Validation_ArrayOf_ce;
+zend_class_entry *Attributes_Validation_Types_ArrayOf_ce;
 
-ZEND_METHOD(Attributes_Validation_ArrayOf, __construct)
+ZEND_METHOD(Attributes_Validation_Types_ArrayOf, __construct)
 {
     zval type_array;
     int num_args;
@@ -30,22 +30,22 @@ ZEND_METHOD(Attributes_Validation_ArrayOf, __construct)
     }
 
     /* Store the array in the $type property */
-    zend_update_property(Attributes_Validation_ArrayOf_ce, Z_OBJ_P(getThis()), "type", sizeof("type") - 1, &type_array);
+    zend_update_property(Attributes_Validation_Types_ArrayOf_ce, Z_OBJ_P(getThis()), "type", sizeof("type") - 1, &type_array);
     zval_ptr_dtor(&type_array);
 }
 
 void attributes_validation_register_ArrayOf_class(void)
 {
     zend_class_entry ce;
-    INIT_NS_CLASS_ENTRY(ce, "Attributes\\Validation\\Types", "ArrayOf", class_Attributes_Validation_ArrayOf_methods);
-    Attributes_Validation_ArrayOf_ce = zend_register_internal_class_ex(&ce, NULL);
+    INIT_NS_CLASS_ENTRY(ce, "Attributes\\Validation\\Types", "ArrayOf", class_Attributes_Validation_Types_ArrayOf_methods);
+    Attributes_Validation_Types_ArrayOf_ce = zend_register_internal_class_ex(&ce, NULL);
     
     /* Register $type property */
-    zend_declare_property_null(Attributes_Validation_ArrayOf_ce, "type", sizeof("type") - 1, ZEND_ACC_PUBLIC);
+    zend_declare_property_null(Attributes_Validation_Types_ArrayOf_ce, "type", sizeof("type") - 1, ZEND_ACC_PUBLIC);
     
     /* Register as an internal attribute that can be used on properties and parameters */
     zend_internal_attribute_register(
-        Attributes_Validation_ArrayOf_ce,
+        Attributes_Validation_Types_ArrayOf_ce,
         ZEND_ATTRIBUTE_TARGET_PROPERTY | ZEND_ATTRIBUTE_TARGET_PARAMETER
     );
 
--- src/arrayof.h
+++ src/arrayof.h
@@ -1,26 +1,26 @@
-#ifndef ATTRIBUTES_VALIDATION_ArrayOf_H
-#define ATTRIBUTES_VALIDATION_ArrayOf_H
+#ifndef ATTRIBUTES_VALIDATION_TYPES_ArrayOf_H
+#define ATTRIBUTES_VALIDATION_TYPES_ArrayOf_H
 
 #include "php.h"
 
 /* Class entry */
-extern zend_class_entry *Attributes_Validation_ArrayOf_ce;
+extern zend_class_entry *Attributes_Validation_Types_ArrayOf_ce;
 
 /* Method declaration */
-ZEND_METHOD(Attributes_Validation_ArrayOf, __construct);
+ZEND_METHOD(Attributes_Validation_Types_ArrayOf, __construct);
 
 /* Argument info for ArrayOf constructor */
-ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Attributes_Validation_ArrayOf___construct, 0, 0, 0)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Attributes_Validation_Types_ArrayOf___construct, 0, 0, 0)
     ZEND_ARG_VARIADIC_TYPE_INFO(0, type, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 /* Method entries */
-static const zend_function_entry class_Attributes_Validation_ArrayOf_methods[] = {
-    ZEND_ME(Attributes_Validation_ArrayOf, __construct, arginfo_class_Attributes_Validation_ArrayOf___construct, ZEND_ACC_PUBLIC)
+static const zend_function_entry class_Attributes_Validation_Types_ArrayOf_methods[] = {
+    ZEND_ME(Attributes_Validation_Types_ArrayOf, __construct, arginfo_class_Attributes_Validation_Types_ArrayOf___construct, ZEND_ACC_PUBLIC)
     ZEND_FE_END
 };
 
 /* Registration function */
 void attributes_validation_register_ArrayOf_class(void);
 
-#endif /* ATTRIBUTES_VALIDATION_ArrayOf_H */
+#endif /* ATTRIBUTES_VALIDATION_TYPES_ArrayOf_H */
