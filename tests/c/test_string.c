75c331ec941a4dafc2df58b197e8a0b122b347a6
Andre.Gil1 <andre.gil@genomicsengland.co.uk>
2026-07-31 16:15:00 +0100

    tests: Add C unit tests for string.c

---
+++ tests/c/test_string.c
@@ -0,0 +1,356 @@
+#include "unity.h"
+#include "helpers/string.h"
+#include <stdlib.h>
+#include <string.h>
+#include "zend_types.h"
+
+static zend_string* zend_string_init(const char *str, size_t len) {
+    zend_string* zs = (zend_string*)malloc(sizeof(zend_string) + len + 1);
+    memcpy(zs->val, str, len);
+    zs->val[len] = '\0';
+    zs->len = len;
+    zs->h = 0;
+    zs->gc.refcount = 1;
+    zs->gc.u.type_info = IS_STRING;
+    return zs;
+}
+
+static zend_string* zend_string_release(zend_string *string) {
+    if (string != NULL) {
+        free(string);
+    }
+}
+
+void setUp(void) {
+    // This is run before EACH test
+}
+
+void tearDown(void) {
+    // This is run after EACH test
+}
+
+// ============= PascalCase Tests =============
+
+void test_to_pascal_case_snake_case(void) {
+    zend_string* input = zend_string_init("first_name", sizeof("first_name") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("FirstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_kebab_case(void) {
+    zend_string* input = zend_string_init("first-name", sizeof("first-name") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("FirstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_space(void) {
+    zend_string* input = zend_string_init("first name", sizeof("first name") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("FirstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_camel_case(void) {
+    zend_string* input = zend_string_init("firstName", sizeof("firstName") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("FirstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_already_pascal(void) {
+    zend_string* input = zend_string_init("FirstName", sizeof("FirstName") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("FirstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_multiple_separators(void) {
+    zend_string* input = zend_string_init("first_name_last-name", sizeof("first_name_last-name") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("FirstNameLastName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_single_word(void) {
+    zend_string* input = zend_string_init("name", sizeof("name") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("Name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_empty(void) {
+    zend_string* input = zend_string_init("", sizeof("") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_pascal_case_numbers(void) {
+    zend_string* input = zend_string_init("user2_name", sizeof("user2_name") - 1);
+    zend_string* result = attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING("User2Name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+// ============= camelCase Tests =============
+
+void test_to_camel_case_snake_case(void) {
+    zend_string* input = zend_string_init("first_name", sizeof("first_name") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("firstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_camel_case_kebab_case(void) {
+    zend_string* input = zend_string_init("first-name", sizeof("first-name") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("firstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_camel_case_pascal_case(void) {
+    zend_string* input = zend_string_init("FirstName", sizeof("FirstName") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("firstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_camel_case_already_camel(void) {
+    zend_string* input = zend_string_init("firstName", sizeof("firstName") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("firstName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_camel_case_single_word(void) {
+    zend_string* input = zend_string_init("Name", sizeof("Name") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_camel_case_multiple_separators(void) {
+    zend_string* input = zend_string_init("first_name_last-name", sizeof("first_name_last-name") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("firstNameLastName", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_camel_case_numbers(void) {
+    zend_string* input = zend_string_init("user2_name", sizeof("user2_name") - 1);
+    zend_string* result = attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING("user2Name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+// ============= snake_case Tests =============
+
+void test_to_snake_case_pascal_case(void) {
+    zend_string* input = zend_string_init("FirstName", sizeof("FirstName") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("first_name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_camel_case(void) {
+    zend_string* input = zend_string_init("firstName", sizeof("firstName") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("first_name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_kebab_case(void) {
+    zend_string* input = zend_string_init("first-name", sizeof("first-name") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("first_name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_space(void) {
+    zend_string* input = zend_string_init("first name", sizeof("first name") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("first_name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_already_snake(void) {
+    zend_string* input = zend_string_init("first_name", sizeof("first_name") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("first_name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_single_word(void) {
+    zend_string* input = zend_string_init("Name", sizeof("Name") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_consecutive_uppercase(void) {
+    zend_string* input = zend_string_init("HTTPResponse", sizeof("HTTPResponse") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("h_t_t_p_response", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_snake_case_numbers(void) {
+    zend_string* input = zend_string_init("user2Name", sizeof("user2Name") - 1);
+    zend_string* result = attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING("user2_name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+// ============= kebab-case Tests =============
+
+void test_to_kebab_case_pascal_case(void) {
+    zend_string* input = zend_string_init("FirstName", sizeof("FirstName") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("first-name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_kebab_case_camel_case(void) {
+    zend_string* input = zend_string_init("firstName", sizeof("firstName") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("first-name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_kebab_case_snake_case(void) {
+    zend_string* input = zend_string_init("first_name", sizeof("first_name") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("first-name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_kebab_case_space(void) {
+    zend_string* input = zend_string_init("first name", sizeof("first name") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("first-name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_kebab_case_already_kebab(void) {
+    zend_string* input = zend_string_init("first-name", sizeof("first-name") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("first-name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_kebab_case_single_word(void) {
+    zend_string* input = zend_string_init("Name", sizeof("Name") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+void test_to_kebab_case_numbers(void) {
+    zend_string* input = zend_string_init("user2Name", sizeof("user2Name") - 1);
+    zend_string* result = attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING("user2-name", result->val);
+    zend_string_release(input);
+    zend_string_release(result);
+}
+
+// ============= Edge Cases =============
+
+void test_no_input_modification(void) {
+    zend_string* input = zend_string_init("first_name", sizeof("first_name") - 1);
+    const char* original_val = strdup(input->val);
+    size_t original_len = input->len;
+    
+    attributes_validation_to_pascal_case(input);
+    TEST_ASSERT_EQUAL_STRING(original_val, input->val);
+    TEST_ASSERT_EQUAL(original_len, input->len);
+    
+    attributes_validation_to_camel_case(input);
+    TEST_ASSERT_EQUAL_STRING(original_val, input->val);
+    TEST_ASSERT_EQUAL(original_len, input->len);
+    
+    attributes_validation_to_snake_case(input);
+    TEST_ASSERT_EQUAL_STRING(original_val, input->val);
+    TEST_ASSERT_EQUAL(original_len, input->len);
+    
+    attributes_validation_to_kebab_case(input);
+    TEST_ASSERT_EQUAL_STRING(original_val, input->val);
+    TEST_ASSERT_EQUAL(original_len, input->len);
+    
+    free((void*)original_val);
+    zend_string_release(input);
+}
+
+// ============= Static Helper Function Tests =============
+
+void test_is_uppercase(void) {
+    TEST_ASSERT_TRUE(is_uppercase('A'));
+    TEST_ASSERT_TRUE(is_uppercase('Z'));
+    TEST_ASSERT_TRUE(is_uppercase('M'));
+    TEST_ASSERT_FALSE(is_uppercase('a'));
+    TEST_ASSERT_FALSE(is_uppercase('z'));
+    TEST_ASSERT_FALSE(is_uppercase('0'));
+    TEST_ASSERT_FALSE(is_uppercase(' '));
+    TEST_ASSERT_FALSE(is_uppercase('!'));
+}
+
+void test_is_lowercase(void) {
+    TEST_ASSERT_TRUE(is_lowercase('a'));
+    TEST_ASSERT_TRUE(is_lowercase('z'));
+    TEST_ASSERT_TRUE(is_lowercase('m'));
+    TEST_ASSERT_FALSE(is_lowercase('A'));
+    TEST_ASSERT_FALSE(is_lowercase('Z'));
+    TEST_ASSERT_FALSE(is_lowercase('0'));
+    TEST_ASSERT_FALSE(is_lowercase(' '));
+    TEST_ASSERT_FALSE(is_lowercase('!'));
+}
+
+void test_is_alphanumeric(void) {
+    // Uppercase letters
+    TEST_ASSERT_TRUE(is_alphanumeric('A'));
+    TEST_ASSERT_TRUE(is_alphanumeric('Z'));
+    // Lowercase letters
+    TEST_ASSERT_TRUE(is_alphanumeric('a'));
+    TEST_ASSERT_TRUE(is_alphanumeric('z'));
+    // Numbers
+    TEST_ASSERT_TRUE(is_alphanumeric('0'));
+    TEST_ASSERT_TRUE(is_alphanumeric('9'));
+    // Non-alphanumeric
+    TEST_ASSERT_FALSE(is_alphanumeric(' '));
+    TEST_ASSERT_FALSE(is_alphanumeric('!'));
+    TEST_ASSERT_FALSE(is_alphanumeric('@'));
+    TEST_ASSERT_FALSE(is_alphanumeric('#'));
+    TEST_ASSERT_FALSE(is_alphanumeric('_'));
+    TEST_ASSERT_FALSE(is_alphanumeric('-'));
+}
